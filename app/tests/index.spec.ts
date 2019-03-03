f6a983a77526c2adf3191ea47859d2f3fc3524ce
Fernando Zhu <fernandoisadev@gmail.com>
2019-03-03 23:31:12 +1300

    Removed dummy unit testing

--- app/tests/index.spec.ts
+++ app/tests/index.spec.ts
@@ -1,7 +1,28 @@
 import { expect } from "chai";
+import * as request from "request";
 
-describe("Test unit testing toolchain", function() {
-    it ("Dummy test", () => {
-        expect(true).equal(true);
-    })
-})
+import { DummyWechatService } from "./dummy-wechat.service";
+
+const baseUrl: string = "http://localhost:4200/";
+const service = new DummyWechatService();
+
+describe("Dummy wechat server tests", () => {
+
+    beforeEach(() => { service.start(); });
+
+    it("Dummy wechat server can be started", () => {
+        request({ url: baseUrl }, (error, response, body) => {
+            expect(body).equal("Wechat server is working.");
+            service.stop();
+        });
+    });
+});
+
+describe("The bot is able to login", () => {
+    beforeEach(() => { service.start(); });
+
+    it("The bot is able to fetch valid UUID", () => {
+        // TODO:
+        service.stop();
+    });
+});
