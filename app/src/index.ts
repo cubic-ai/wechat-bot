e1b451da473b40faf31fede2821b91f88019618e
Fernando Zhu <fernandoisadev@gmail.com>
2019-03-04 21:58:31 +1300

    Removed testing code snippets

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,15 +1,11 @@
 import { Logger } from "./logger";
 
-// import { CBotConfig } from "./interface";
-// import { WechatBot } from "./wechat-bot";
+import { CBotConfig } from "./interface";
+import { WechatBot } from "./wechat-bot";
 
-// async function main() {
-//     const bot = new WechatBot(CBotConfig);
-//     await bot.login();
-// }
+async function main() {
+    const bot = new WechatBot(CBotConfig);
+    await bot.login();
+}
 
-// main();
-
-const logger = new Logger();
-logger.info("Hello world");
-logger.error("error");
+main();
