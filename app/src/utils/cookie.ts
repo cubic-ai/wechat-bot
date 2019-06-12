0b07edc4247d6a11d285f641c8300b957abf9958
Fernando Zhu <fernandoisadev@gmail.com>
2019-06-12 23:35:43 +1200

    Run into message sync issue, start adding a cookie class

---
+++ app/src/utils/cookie.ts
@@ -0,0 +1,12 @@
+class Cookie {
+    public static json: any;
+    public static captureCookieFromResponseData(data: any) {
+        //
+    }
+    public static clear() {
+        //
+    }
+    public static toString(): string {
+        return;
+    }
+}
